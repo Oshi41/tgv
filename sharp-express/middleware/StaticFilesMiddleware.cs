2b4c82c75ae518f1dec63117820a49722d5b90a2
Oshi41 <sheglovaoe@gmail.com>
2024-11-13 03:24:03 +0300

    static files handling

---
+++ sharp-express/middleware/StaticFilesMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Net;
+using MimeTypes;
+using sharp_express.core;
+
+namespace sharp_express.middleware;
+
+public class StaticFilesConfig
+{
+    public bool FallThrough { get; set; } = true;
+    public string SourceDirectory { get; set; }
+}
+
+public static class StaticFilesMiddleware
+{
+    public static Express ServeStatic(this Express express, StaticFilesConfig cfg)
+    {
+        async Task Middleware(IContext ctx, Action next, Exception? e)
+        {
+            if (ctx.Stage != HandleStages.Handle)
+            {
+                next();
+                return;
+            }
+
+            if (ctx.HttpMethod is not "GET" and not "HEAD")
+            {
+                if (cfg.FallThrough)
+                {
+                    next();
+                    return;
+                }
+
+                ctx.ResponseHeaders["Content-Length"] = "0";
+                ctx.ResponseHeaders["Allow"] = "GET, HEAD";
+                ctx.Send(HttpStatusCode.MethodNotAllowed);
+                return;
+            }
+
+            if (!Directory.Exists(cfg.SourceDirectory))
+            {
+                await Console.Error.WriteLineAsync($"Source directory {cfg.SourceDirectory} does not exist");
+                throw ctx.Throw(HttpStatusCode.InternalServerError);
+            }
+            
+            var file = Path.GetFullPath(Path.Join(cfg.SourceDirectory, ctx.Url.AbsolutePath));
+            if (!File.Exists(file))
+            {
+                next();
+                return;
+            }
+            
+            if (!file.StartsWith(Path.GetFullPath(cfg.SourceDirectory)))
+            {
+                Debug.WriteLine("Attempt to get file outside of the source directory: {0}", file);
+                throw ctx.Throw(HttpStatusCode.BadRequest);
+            }
+            
+            var bytes = await File.ReadAllBytesAsync(file);
+            ctx.Send(bytes);
+            ctx.ContentType = MimeTypeMap.GetMimeType(Path.GetExtension(file));
+        }
+
+        express.After(Middleware);
+        return express;
+    }
+}
