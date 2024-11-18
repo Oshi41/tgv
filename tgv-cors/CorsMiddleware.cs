1beab7fd705a672401e7cf0b21f89671e117d118
Oshi41 <sheglovaoe@gmail.com>
2024-11-18 22:54:10 +0300

    Cors introduced

---
+++ tgv-cors/CorsMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Net.Http;
+using tgv.core;
+
+namespace tgv_cors;
+
+public static class CorsMiddleware
+{
+    public static Handle Cors(CorsSettings? settings = null) => async (ctx, next, _) =>
+    {
+        settings ??= new CorsSettings(["*"]);
+        var origin = ctx.ClientHeaders["Origin"];
+
+        // no origin founded
+        if (string.IsNullOrEmpty(origin))
+        {
+            next();
+            return;
+        }
+        
+        if (await settings.ValidateOrigin(ctx))
+            ctx.ResponseHeaders["Access-Control-Allow-Origin"] = origin;
+        if (settings.UseCredentials)
+            ctx.ResponseHeaders["Access-Control-Allow-Credentials"] = "true";
+        if (settings.ExposedHeaders?.Any() == true)
+            ctx.ResponseHeaders["Access-Control-Expose-Headers"] = string.Join(",", settings.ExposedHeaders);
+
+        // continue all request except preflight
+        if (ctx.Method != HttpMethod.Options)
+        {
+            next();
+            return;
+        }
+        
+        // preflight
+        if (settings.Methods?.Any() == true)
+            ctx.ResponseHeaders["Access-Control-Allow-Methods"] = string.Join(",", settings.Methods.Select(x => x.Method));
+        if (settings.MaxAge != null)
+            ctx.ResponseHeaders["Access-Control-Max-Age"] = settings.MaxAge.Value.ToString();
+        if (settings.AllowedHeaders?.Any() == true)
+            ctx.ResponseHeaders["Access-Control-Allow-Headers"] = string.Join(",", settings.AllowedHeaders);
+
+        // continue routing
+        if (settings.ContinuePreflight)
+        {
+            next();
+            return;
+        }
+
+        ctx.ResponseHeaders["Content-Length"] = "0";
+        await ctx.Send(settings.Code);
+    };
+}
---
+++ tgv-cors/CorsSettings.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using tgv.core;
+using tgv.extensions;
+
+namespace tgv_cors;
+
+public class CorsSettings
+{
+    /// <summary>
+    /// Default Cors settings ctor
+    /// </summary>
+    /// <param name="validateOrigin">Validate origin callback. <p/> Always true by default</param>
+    /// <param name="methods">List of HTTP method supporting by CORS. <p/>
+    /// GET, POST, PUT, HEAD, DELETE, PATCH by default</param>
+    /// <param name="continuePreflight">Should continue routing on preflight requests? <p/>
+    /// False by default</param>
+    /// <param name="code">Code using to response on preflight requests. <p/>
+    /// 204 NoContent by default</param>
+    /// <param name="useCredentials">Should use credentials? <p/>
+    /// False by default</param>
+    /// <param name="allowedHeaders">List of allowed headers <p/>
+    /// Not sending it by default, list is empty</param>
+    /// <param name="exposedHeaders">List of exposed headers <p/>
+    /// Not sending it by default, list is empty</param>
+    public CorsSettings(Func<Context, Task<bool>>? validateOrigin,
+        HttpMethod[]? methods,
+        bool continuePreflight,
+        HttpStatusCode? code,
+        uint? maxAge,
+        bool useCredentials,
+        string[]? allowedHeaders,
+        string[]? exposedHeaders)
+    {
+        ValidateOrigin = validateOrigin ?? (_ => Task.FromResult(true));
+        Methods = methods ??
+        [
+            HttpMethod.Get, HttpMethod.Post, HttpMethod.Put, HttpMethod.Head, HttpMethod.Delete,
+            HttpMethodExtensions.Patch
+        ];
+        ContinuePreflight = continuePreflight;
+        Code = code ?? HttpStatusCode.NoContent;
+        UseCredentials = useCredentials;
+        AllowedHeaders = allowedHeaders ?? [];
+        ExposedHeaders = exposedHeaders ?? [];
+        MaxAge = maxAge;
+    }
+
+    public CorsSettings(
+        string[]? origins = default,
+        HttpMethod[]? methods = default,
+        bool continuePreflight = false,
+        HttpStatusCode code = HttpStatusCode.NoContent,
+        uint? maxAge = null,
+        bool useCredentials = false,
+        string[]? allowedHeaders = default,
+        string[]? exposedHeaders = default
+    )
+        : this(Create(origins),
+            methods,
+            continuePreflight,
+            code,
+            maxAge,
+            useCredentials,
+            allowedHeaders,
+            exposedHeaders)
+    {
+    }
+
+    public CorsSettings(
+        Regex[]? matchers = default,
+        HttpMethod[]? methods = default,
+        bool continuePreflight = false,
+        HttpStatusCode code = HttpStatusCode.NoContent,
+        uint? maxAge = null,
+        bool useCredentials = false,
+        string[]? allowedHeaders = default,
+        string[]? exposedHeaders = default
+    )
+        : this(Create(matchers),
+            methods,
+            continuePreflight,
+            code,
+            maxAge,
+            useCredentials,
+            allowedHeaders,
+            exposedHeaders)
+    {
+    }
+
+    private static Func<Context, Task<bool>> Create(string[]? origins)
+    {
+        if (origins?.Any() != true)
+        {
+            return _ => Task.FromResult(true);
+        }
+
+        return ctx =>
+        {
+            var origin = ctx.ClientHeaders["Origin"];
+            return Task.FromResult(origins.Contains(origin));
+        };
+    }
+
+    private static Func<Context, Task<bool>> Create(Regex[]? matchers)
+    {
+        if (matchers?.Any() != true)
+        {
+            return _ => Task.FromResult(true);
+        }
+
+        return ctx =>
+        {
+            var origin = ctx.ClientHeaders["Origin"];
+            return Task.FromResult(matchers.Any(x => x.IsMatch(origin)));
+        };
+    }
+
+    public HttpMethod[] Methods { get; }
+    public string[] AllowedHeaders { get; }
+    public string[] ExposedHeaders { get; }
+    public bool ContinuePreflight { get; }
+    public bool UseCredentials { get; }
+    public HttpStatusCode Code { get; }
+    public Func<Context, Task<bool>> ValidateOrigin { get; }
+    public uint? MaxAge { get;}
+}
